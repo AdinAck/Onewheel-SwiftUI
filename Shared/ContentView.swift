0a1ead93dde4135a3ac16e1dfc891a1ce5ed26b5
Adin Ackerman <adinackerman@gmail.com>
2022-08-30 15:54:46 -0700

    Initial Commit

---
+++ Shared/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Shared
+//
+//  Created by Adin Ackerman on 8/30/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Shared/Onewheel_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  Onewheel_SwiftUIApp.swift
+//  Shared
+//
+//  Created by Adin Ackerman on 8/30/22.
+//
+
+import SwiftUI
+
+@main
+struct Onewheel_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
